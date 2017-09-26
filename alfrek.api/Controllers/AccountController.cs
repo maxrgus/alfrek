1dd10a2f767c936a2e2c08c2ace95ba9fa8ef0d2
Maximilian Gustafsson <maxrgus@Maximilians-MacBook-Pro.local>
2017-09-26 14:40:13 +0200

    Added register, signin and signout

---
+++ alfrek.api/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using alfrek.api.Models;
+using alfrek.api.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace alfrek.api.Controllers
+{
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string email, string password)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = new ApplicationUser
+            {
+                UserName = email,
+                Email = email,
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+            
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.Select(x => x.Description).ToList());
+            }
+
+            await _signInManager.SignInAsync(user, false);
+            
+            return Ok(user);
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, false);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            return Ok("Signed in");
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok("Signed out");
+        }
+    }
+}
